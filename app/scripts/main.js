df47f560ac0a1f32fb249bd7999fa1daf02da28b
ChrissiQ <rediahs@gmail.com>
2017-07-28 22:58:30 -0600

    Sizing and button to start.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,49 @@
 let stage;
 
 function maxSize() {
-  stage.canvas.setAttribute('width', window.innerWidth);
-  stage.canvas.setAttribute('height', window.innerHeight);
+  let maxWidth = window.innerWidth;
+  let maxHeight = window.innerHeight;
+  let width;
+  let height;
+  let tryWidth = maxHeight * (9 / 16);
+  let tryHeight = maxWidth * (16 / 9);
+  if (tryWidth < maxWidth) {
+    width = tryWidth;
+    height = maxHeight;
+  } else {
+    width = maxWidth;
+    height = tryHeight;
+  }
+  stage.canvas.setAttribute('width', width);
+  stage.canvas.setAttribute('height', height);
 }
 
 function init() {
   stage = new createjs.Stage('game');
   maxSize();
+  if (stage.canvas.webkitRequestFullscreen) {
+    stage.canvas.webkitRequestFullscreen();
+  } else if (stage.canvas.mozRequestFullScreen) {
+    stage.canvas.mozRequestFullScreen();
+  } else if (stage.canvas.requestFullscreen) {
+    stage.canvas.requestFullscreen();
+  }
+  stage.canvas.style.backgroundColor = 'green';
 }
 
 init();
+
+window.addEventListener('resize', () => {
+  maxSize();
+});
+
+let button = document.getElementById('play');
+button.addEventListener('click', () => {
+  if (stage.canvas.webkitRequestFullscreen) {
+    stage.canvas.webkitRequestFullscreen();
+  } else if (stage.canvas.mozRequestFullScreen) {
+    stage.canvas.mozRequestFullScreen();
+  } else if (stage.canvas.requestFullscreen) {
+    stage.canvas.requestFullscreen();
+  }
+});
